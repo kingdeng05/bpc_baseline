23aa3dcca0eaee040441c4992779a345e168fcca
Fuheng Deng <kingdeng05@gmail.com>
2025-03-11 23:36:47 -0700

    updated the key path

--- install_gcsfuse.sh
+++ install_gcsfuse.sh
@@ -4,4 +4,4 @@ curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install -y gcsfuse
 mkdir -p /content/gcs-bucket
-gcsfuse --implicit-dirs --key-file static-nomad-452701-n4-42f2015247b3.json bin-picking-challenge-2025 /content/gcs-bucket
+gcsfuse --implicit-dirs --key-file /content/static-nomad-452701-n4-42f2015247b3.json bin-picking-challenge-2025 /content/gcs-bucket
